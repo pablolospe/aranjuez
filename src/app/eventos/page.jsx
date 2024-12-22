a258605d1fcb90df643c520ffc6cd17a36c4472e
pablolospe <yosoypxl@gmail.com>
2024-12-22 00:32:58 -0300

    gotcha

--- src/app/eventos/page.jsx
+++ src/app/eventos/page.jsx
@@ -9,14 +9,14 @@ function EventosPage() {
           src="/berna.jpg"
           alt="Background"
           fill
-          style={{ objectFit: "cover" }}
+          style={{ objectFit: "contain" }}
           priority
-          className="h-full"
+          className="h-full w-full"
         />
         <div className="absolute inset-0 bg-black bg-opacity-40" />
       </div>
-      <div className="relative h-full flex items-center justify-center text-center px-4">
-        <h1 className="text-4xl md:text-5xl lg:text-6xl font-serif text-white">GOTCHA!</h1>
+      <div className="relative h-full flex items-end justify-center text-center px-4">
+        <h1 className="text-6xl font-bold text-white">GOTCHA!</h1>
       </div>
     </div>
   );
